6c1356650e32b308a61bdb4c9c8c96051db040b4
Luis Jorge <gevolutionxalapa@gmail.com>
2025-05-20 23:01:48 -0600

    Configuracion basica

---
+++ storage/framework/views/3356c1371b04e85d0c52783c7ebed529.php
@@ -0,0 +1,14 @@
+<footer class="footer">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-sm-6">
+                <script>document.write(new Date().getFullYear())</script> © Skote.
+            </div>
+            <div class="col-sm-6">
+                <div class="text-sm-end d-none d-sm-block">
+                    Design & Develop by Themesbrand
+                </div>
+            </div>
+        </div>
+    </div>
+</footer><?php /**PATH /Users/lpablomac/Downloads/Skote_Html_Laravel_v4.3.0/Laravel/Admin/resources/views/layouts/footer.blade.php ENDPATH**/ ?>
