6139f92bac25c8eaff9d7653c323d3cf0e1211be
Luis Jorge (MAC) <gevolutionxalapa@gmail.com>
2025-06-02 01:37:24 -0600

    Mostrando el avance de fraccionamientos

--- resources/views/pages/gestion-fraccionamientos/modal/edit.blade.php
+++ resources/views/pages/gestion-fraccionamientos/modal/edit.blade.php
@@ -6,7 +6,7 @@
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
-                <form method="POST" action="{{ route('proyectos.update', ['proyecto' => $fracc->id]) }}" autocomplete="off">
+                <form method="POST" action="{{ route('fraccionamiento.update', ['fraccionamiento' => $fracc->id]) }}" autocomplete="off">
                     @csrf
                     @method('PUT')
                     @include('pages.gestion-fraccionamientos.formulario.fraccionamiento')
--- storage/framework/views/5398dd681376fdd619c982ed2bdd1dd5.php
+++ storage/framework/views/5398dd681376fdd619c982ed2bdd1dd5.php
@@ -1,7 +1,7 @@
  <div class="row mb-3">
         <div class="col-md-6">
             <label for="nombre" class="form-label">Nombre</label>
-            <input type="text" name="nombre" id="nombre" class="form-control" required>
+            <input type="text" name="nombre" id="nombre" class="form-control" required value="<?php echo e(isset($fracc)? $fracc->nombre:''); ?>">
         </div>
 
         <div class="col-md-6">
@@ -13,31 +13,31 @@
     <div class="row mb-3">
         <div class="col-md-6">
             <label for="reponsable" class="form-label">Responsable</label>
-            <input type="text" name="reponsable" id="reponsable" class="form-control" required>
+            <input type="text" name="reponsable" id="reponsable" class="form-control" required value="<?php echo e(isset($fracc)? $fracc->reponsable:''); ?>">
         </div>
 
         <div class="col-md-6">
             <label for="propietaria" class="form-label">Propietaria</label>
-            <input type="text" name="propietaria" id="propietaria" class="form-control" required>
+            <input type="text" name="propietaria" id="propietaria" class="form-control" required value="<?php echo e(isset($fracc)? $fracc->propietaria:''); ?>">
         </div>
     </div>
 
     <div class="row mb-3">
         <div class="col-md-6">
             <label for="predio_urbano" class="form-label">Predio Urbano</label>
-            <input type="text" name="predio_urbano" id="predio_urbano" class="form-control">
+            <input type="text" name="predio_urbano" id="predio_urbano" class="form-control" value="<?php echo e(isset($fracc)? $fracc->predio_urbano:''); ?>">
         </div>
 
         <div class="col-md-6">
             <label for="superficie" class="form-label">Superficie (m²)</label>
-            <input type="number" step="0.01" name="superficie" id="superficie" class="form-control">
+            <input type="number" step="0.01" name="superficie" id="superficie" class="form-control" value="<?php echo e(isset($fracc)? $fracc->superficie:''); ?>">
         </div>
     </div>
 
    <div class="row mb-3">
     <div class="col-md-6">
         <label for="ubicacion" class="form-label">Ubicación</label>
-        <input type="text" name="ubicacion" id="ubicacion" class="form-control">
+        <input type="text" name="ubicacion" id="ubicacion" class="form-control" value="<?php echo e(isset($fracc)? $fracc->ubicacion:''); ?>">
     </div>
 
     <div class="col-md-6">
@@ -51,7 +51,7 @@
 </div>
     <div class="mb-3">
         <label for="observaciones" class="form-label">Observaciones</label>
-        <textarea name="observaciones" id="observaciones" class="form-control" rows="3"></textarea>
+        <textarea name="observaciones" id="observaciones" class="form-control" rows="3"> <?php echo e(isset($fracc)? $fracc->observaciones:''); ?> </textarea>
     </div>
 
    <?php /**PATH /Users/luisjorgepablosartillo/Documents/PROYECTOS/LoteGest/resources/views/pages/gestion-fraccionamientos/formulario/fraccionamiento.blade.php ENDPATH**/ ?>
