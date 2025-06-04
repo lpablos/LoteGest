096dd96fb609353d0e89b22750028fc38478568f
Luis Jorge (MAC) <gevolutionxalapa@gmail.com>
2025-06-04 16:41:19 -0600

    Mostrar los datos en el fomulario lotes

--- resources/views/pages/gestion-lotes/formulario/lote.blade.php
+++ resources/views/pages/gestion-lotes/formulario/lote.blade.php
@@ -2,33 +2,33 @@
 
     <div class="col-md-4">
         <label for="frente_m" class="form-label">Frente (m)</label>
-        <input type="number" step="0.01" name="frente_m" id="frente_m{{$lote->id ?? ''}}" class="form-control" required>
+        <input type="number" step="0.01" name="frente_m" id="frente_m{{$lote->id ?? ''}}" class="form-control"  value="{{ isset($lote)? $lote->frente_m : ''}}" required>
     </div>
 
     <div class="col-md-4">
         <label for="fondo_m" class="form-label">Fondo (m)</label>
-        <input type="number" step="0.01" name="fondo_m" id="fondo_m{{$lote->id ?? ''}}" class="form-control" required>
+        <input type="number" step="0.01" name="fondo_m" id="fondo_m{{$lote->id ?? ''}}" class="form-control" value="{{ isset($lote)? $lote->fondo_m : ''}}" required>
     </div>
     @if (isset($lote))
         <div class="col-md-4">
             <label for="superficie_m2" class="form-label">Superficie (m²)</label>
-            <input type="number" step="0.01" name="superficie_m2" id="superficie_m2{{ $lote->id ?? ''}}" class="form-control">
+            <input type="number" step="0.01" name="superficie_m2" id="superficie_m2{{ $lote->id ?? ''}}" value="{{ isset($lote)? $lote->superficie_m2 : ''}}" class="form-control" {{ isset($lote) ? 'readonly' : '' }}>
         </div>        
     @endif
 
     <div class="col-md-6">
         <label for="precio_contado" class="form-label">Precio Contado</label>
-        <input type="number" step="0.01" name="precio_contado" id="precio_contado{{ $lote->id ?? '' }}" class="form-control" required>
+        <input type="number" step="0.01" name="precio_contado" id="precio_contado{{ $lote->id ?? '' }}" value="{{ isset($lote)? $lote->precio_contado : ''}}" class="form-control" required>
     </div>
 
     <div class="col-md-6">
         <label for="precio_credito" class="form-label">Precio Crédito</label>
-        <input type="number" step="0.01" name="precio_credito" id="precio_credito{{ $lote->id ?? '' }}" class="form-control" required>
+        <input type="number" step="0.01" name="precio_credito" id="precio_credito{{ $lote->id ?? '' }}" value="{{ isset($lote)? $lote->precio_credito : ''}}" class="form-control" required>
     </div>
 
     <div class="col-md-6">
         <label for="plano" class="form-label">Plano (Archivo)</label>
-        <input type="file" name="plano" id="plano{{$lote->id ?? ''}}" class="form-control">
+        <input type="file" name="plano" id="plano{{$lote->id ?? ''}}" value="{{ isset($lote)? $lote->plano : ''}}" class="form-control">
     </div>
 
     <div class="col-md-6">
@@ -36,7 +36,10 @@
         <select name="manzana_id" id="manzana_id{{ $lote->id ?? '' }}" class="form-select" required>
             <option value="" selected disabled>Selecciona una manzana</option>
             @foreach($fracc->manzanas as $manzana)
-                <option value="{{ $manzana->id }}">Manzana {{ $manzana->num_lotes }}</option>
+                <option value="{{ $manzana->id }}"
+                    {{ isset($lote) && $lote->manzana_id == $manzana->id ? 'selected' : '' }}>
+                    Manzana {{ $manzana->num_lotes }}
+                </option>
             @endforeach
         </select>
     </div>
@@ -46,7 +49,10 @@
         <select name="cat_estatus_id" id="cat_estatus_id{{ $lote->id ?? '' }}" class="form-select" required>
             <option value="" selected disabled>Selecciona un estatus</option>
             @foreach($estatus as $estatusItem)
-                <option value="{{ $estatusItem->id }}">{{ $estatusItem->nombre }}</option>
+                <option value="{{ $estatusItem->id }}"
+                    {{ isset($lote) && $lote->cat_estatus_id == $estatusItem->id ? 'selected' : '' }}>
+                    {{ $estatusItem->nombre }}
+                </option>
             @endforeach
         </select>
     </div>
--- storage/framework/views/49a9a829f4f1dd599d247061688f73cf.php
+++ storage/framework/views/49a9a829f4f1dd599d247061688f73cf.php
@@ -2,33 +2,33 @@
 
     <div class="col-md-4">
         <label for="frente_m" class="form-label">Frente (m)</label>
-        <input type="number" step="0.01" name="frente_m" id="frente_m<?php echo e($lote->id ?? ''); ?>" class="form-control" required>
+        <input type="number" step="0.01" name="frente_m" id="frente_m<?php echo e($lote->id ?? ''); ?>" class="form-control"  value="<?php echo e(isset($lote)? $lote->frente_m : ''); ?>" required>
     </div>
 
     <div class="col-md-4">
         <label for="fondo_m" class="form-label">Fondo (m)</label>
-        <input type="number" step="0.01" name="fondo_m" id="fondo_m<?php echo e($lote->id ?? ''); ?>" class="form-control" required>
+        <input type="number" step="0.01" name="fondo_m" id="fondo_m<?php echo e($lote->id ?? ''); ?>" class="form-control" value="<?php echo e(isset($lote)? $lote->fondo_m : ''); ?>" required>
     </div>
     <?php if(isset($lote)): ?>
         <div class="col-md-4">
             <label for="superficie_m2" class="form-label">Superficie (m²)</label>
-            <input type="number" step="0.01" name="superficie_m2" id="superficie_m2<?php echo e($lote->id ?? ''); ?>" class="form-control">
+            <input type="number" step="0.01" name="superficie_m2" id="superficie_m2<?php echo e($lote->id ?? ''); ?>" value="<?php echo e(isset($lote)? $lote->superficie_m2 : ''); ?>" class="form-control" <?php echo e(isset($lote) ? 'readonly' : ''); ?>>
         </div>        
     <?php endif; ?>
 
     <div class="col-md-6">
         <label for="precio_contado" class="form-label">Precio Contado</label>
-        <input type="number" step="0.01" name="precio_contado" id="precio_contado<?php echo e($lote->id ?? ''); ?>" class="form-control" required>
+        <input type="number" step="0.01" name="precio_contado" id="precio_contado<?php echo e($lote->id ?? ''); ?>" value="<?php echo e(isset($lote)? $lote->precio_contado : ''); ?>" class="form-control" required>
     </div>
 
     <div class="col-md-6">
         <label for="precio_credito" class="form-label">Precio Crédito</label>
-        <input type="number" step="0.01" name="precio_credito" id="precio_credito<?php echo e($lote->id ?? ''); ?>" class="form-control" required>
+        <input type="number" step="0.01" name="precio_credito" id="precio_credito<?php echo e($lote->id ?? ''); ?>" value="<?php echo e(isset($lote)? $lote->precio_credito : ''); ?>" class="form-control" required>
     </div>
 
     <div class="col-md-6">
         <label for="plano" class="form-label">Plano (Archivo)</label>
-        <input type="file" name="plano" id="plano<?php echo e($lote->id ?? ''); ?>" class="form-control">
+        <input type="file" name="plano" id="plano<?php echo e($lote->id ?? ''); ?>" value="<?php echo e(isset($lote)? $lote->plano : ''); ?>" class="form-control">
     </div>
 
     <div class="col-md-6">
@@ -36,7 +36,11 @@
         <select name="manzana_id" id="manzana_id<?php echo e($lote->id ?? ''); ?>" class="form-select" required>
             <option value="" selected disabled>Selecciona una manzana</option>
             <?php $__currentLoopData = $fracc->manzanas; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $manzana): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
-                <option value="<?php echo e($manzana->id); ?>">Manzana <?php echo e($manzana->num_lotes); ?></option>
+                <option value="<?php echo e($manzana->id); ?>"
+                    <?php echo e(isset($lote) && $lote->manzana_id == $manzana->id ? 'selected' : ''); ?>>
+                    Manzana <?php echo e($manzana->num_lotes); ?>
+
+                </option>
             <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
         </select>
     </div>
@@ -46,7 +50,11 @@
         <select name="cat_estatus_id" id="cat_estatus_id<?php echo e($lote->id ?? ''); ?>" class="form-select" required>
             <option value="" selected disabled>Selecciona un estatus</option>
             <?php $__currentLoopData = $estatus; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $estatusItem): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
-                <option value="<?php echo e($estatusItem->id); ?>"><?php echo e($estatusItem->nombre); ?></option>
+                <option value="<?php echo e($estatusItem->id); ?>"
+                    <?php echo e(isset($lote) && $lote->cat_estatus_id == $estatusItem->id ? 'selected' : ''); ?>>
+                    <?php echo e($estatusItem->nombre); ?>
+
+                </option>
             <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
         </select>
     </div>
