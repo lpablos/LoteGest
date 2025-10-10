c4ce7798e541fce681b59878ccc47ae7a4b37765
Luis Jorge (MAC) <gevolutionxalapa@gmail.com>
2025-10-09 21:59:44 -0600

    COntrato avance

--- storage/framework/views/67dec3b2c5428e457b9c786a31206a98.php
+++ storage/framework/views/67dec3b2c5428e457b9c786a31206a98.php
@@ -14,6 +14,90 @@
         type="text/css" />
     <link rel="stylesheet" href="<?php echo e(URL::asset('build/libs/@chenfengyuan/datepicker/datepicker.min.css')); ?>">
     <link href="<?php echo e(URL::asset('build/libs/dropzone/dropzone.css')); ?>" rel="stylesheet" type="text/css" />
+    <style>
+        body {
+            font-family: DejaVu Sans, sans-serif;
+            font-size: 12px;
+            line-height: 1.4;
+            margin: 20px;
+            text-align: justify; /* Justificado */
+        }
+        h1 {
+            text-align: center;
+            font-size: 16px;
+            margin-bottom: 10px;
+        }
+        h2 {
+            text-align: center;
+            font-size: 16px;
+            margin-bottom: 10px;
+        }
+        h3 {
+            text-align: center;
+            /* font-size: 16px; */
+            /* margin-bottom: 10px; */
+        }
+
+       .text-right {
+            text-align: right;
+        }
+
+        hr {
+            border: 1px dashed #000;
+            margin: 15px 0;
+        }
+        .clausula {
+            margin-bottom: 15px;
+        }
+        .firma {
+            margin-top: 50px;
+            width: 100%;
+            display: flex;
+            justify-content: space-between;
+        }
+        .firma div {
+            text-align: center;
+        }
+        .observaciones {
+            margin-top: 20px;
+        }
+        table {
+            width: 100%;
+            border-collapse: collapse;
+            margin: 10px 0;
+        }
+        table, th, td {
+            border: 1px solid #000;
+        }
+        th, td {
+            padding: 6px;
+            text-align: center;
+        }
+
+        .tabla-firmas {
+            width: 100%;
+            border-collapse: collapse;
+        }
+
+        .tabla-firmas .celda {
+            width: 50%;
+            padding: 8px;
+            vertical-align: middle;
+        }
+
+        .tabla-firmas .vacia {
+            height: 30px; /* espacio para firma */
+            border-bottom: 1px solid #000;
+        }
+
+        .tabla-firmas .vendedor {
+            text-align: center;
+        }
+
+        .tabla-firmas .comprador {
+            text-align: center;
+        }
+    </style>
 <?php $__env->stopSection(); ?>
 
 <?php $__env->startSection('content'); ?>
@@ -86,6 +170,9 @@
                                     <label for="entidad_id"> Corredor </label>
                                     <select class="form-select form-select-sm" id="entidad_id" name="entidad_id" style="cursor: pointer;" required>
                                         <option value="" selected disabled> Selecciona una opción </option>
+                                        <?php $__currentLoopData = $corredores; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $corredor): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                                            <option value="<?php echo e($corredor->id); ?>">- <?php echo e($corredor->full_name); ?></option>
+                                        <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
                                     </select>
                                 </div>
                             </div>
@@ -112,8 +199,8 @@
                                     <label for="mpio_id"> Fraccionamiento </label>
                                     <select class="form-select form-select-sm" id="mpio_id" name="mpio_id" style="cursor: pointer;" required>
                                         <option value="" selected disabled> Selecciona una opción </option>
-                                        <?php $__currentLoopData = $mpios; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $mpio): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
-                                            <option value="<?php echo e($mpio->id); ?>">- <?php echo e($mpio->nom_mpio); ?></option>
+                                        <?php $__currentLoopData = $fraccionamientos; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $fraccionamiento): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                                            <option value="<?php echo e($fraccionamiento->id); ?>">- <?php echo e($fraccionamiento->nombre); ?></option>
                                         <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
                                     </select>
                                 </div>
@@ -190,19 +277,135 @@
                     </section>
 
                     <!-- Bank Details -->
-                    <h3> Sección 3 </h3>
+                   <h3> Datos del Contrato </h3>
                     <section>
-                        <div>
-                            <form>
-                                <div class="row">
-                                    
-                                </div>
-                            </form>
-                        </div>
+                        <form>
+                           <h4>CONTRATO DE COMPRAVENTA</h4>
+                            <p>Contrato de la compraventa que celebran por una parte como VENDEDOR 
+                                <input type="text" name="vendedor_asc" id="vendedor_asc" value="FAMILIA PINEDA" class="form-control form-control-sm" style="width: 200px; display: inline-block; vertical-align: middle; margin: 0 4px;">
+                            quienes son <strong>propietarios/copropietarios</strong> y que son representados por 
+                                <input type="text" name="propietarios_asc" id="propietarios_asc" value="LA ARQ TANIA MEDINA MARCOS" class="form-control form-control-sm" style="width: 200px; display: inline-block; vertical-align: middle; margin: 0 4px;">
+                            y por el otro lado como COMPRADOR(A): 
+                                <input type="text" name="comprador_asc" id="comprador_asc" value="LA ARQ TANIA MEDINA MARCOS" class="form-control form-control-sm" style="width: 200px; display: inline-block; vertical-align: middle; margin: 0 4px;">
+                            los cuales se sujetan al tenor de las siguientes clausulas:</p>
+
+                            <h5 class="text-center"> ------------------------------------ ANTECEDENTES ------------------------------------ </h5>
+
+                            <p>La familia 
+                                <input type="text" name="propietarios_asc" id="propietarios_asc" value="FAMILIA PINEDA" class="form-control form-control-sm" style="width: 200px; display: inline-block; vertical-align: middle; margin: 0 4px;">
+                            son <strong>propietarios/copropietarios</strong> de un predio identificado con
+                                <input type="text" name="ubicacion_asc" id="ubicacion_asc" value="Escritura numero 1,354 amparado bajo el numero de resgitro 13, tomo 14, pasada ante notario publico numero 3 de esta Ciudad de Zacapu, Michoacan" class="form-control form-control-sm" style="width: 410px; display: inline-block; vertical-align: middle; margin: 0 4px;">
+                            ubicado 
+                                <input type="text" name="ubicacion_zona_asc" id="ubicacion_zona_asc" value="AL ORIENTE DE LA CIUDAD DE ZACAPU denominado como LA CIENEGA" class="form-control form-control-sm" style="width: 250px; display: inline-block; vertical-align: middle; margin: 0 4px;">
+                            municipio de 
+                                <input type="text" name="municipio_estado_asc" id="municipio_estado_asc" value="ZACAPU, MICHOACAN" class="form-control form-control-sm" style="width: 250px; display: inline-block; vertical-align: middle; margin: 0 4px;">.</p>
+
+                            <h5 class="text-center"> ------------------------------------ CLAUSULAS ------------------------------------ </h5>
+
+                            <h6>PRIMERA. - </h6>
+                            <p>La 
+                                <input type="text" name="propietarios_uno_asc" id="propietarios_uno_asc" value="FAMILIA PINEDA" class="form-control form-control-sm" style="width: 200px; display: inline-block; vertical-align: middle; margin: 0 4px;"> 
+                            venden una fracción marcada con el número de lote 
+                                <input type="text" name="lotes_uno_asc" id="lotes_uno_asc" value="3, 4, 5" class="form-control form-control-sm" style="width: 150px; display: inline-block; vertical-align: middle; margin: 0 4px;"> 
+                            que se describe con las siguientes medidas, colindancias y superficie:</p>
+
+
+                            <table style="margin: 0 auto; width: 60%; border-collapse: collapse; text-align: center;">
+                                <thead>
+                                    <tr>
+                                    <th style="border: 1px solid #000; padding: 6px;">Orientación</th>
+                                    <th style="border: 1px solid #000; padding: 6px;">Medida</th>
+                                    <th style="border: 1px solid #000; padding: 6px;">Colindancia</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td style="border: 1px solid #000; padding: 6px;">Noroeste</td>
+                                        <td style="border: 1px solid #000; padding: 6px;">
+                                            <input type="text" name="viento1_medida" id="viento1_medida" value="24.00 M" class="form-control form-control-sm" style="width: 150px; display: inline-block; vertical-align: middle; margin: 0 4px;">
+                                        </td>
+                                        <td style="border: 1px solid #000; padding: 6px;">
+                                            <input type="text" name="viento1_colinda" id="viento1_colinda" value="Lote 2" class="form-control form-control-sm" style="width: 150px; display: inline-block; vertical-align: middle; margin: 0 4px;">
+                                        </td>
+                                        </tr>
+                                        <tr>
+                                        <td style="border: 1px solid #000; padding: 6px;">Sureste</td>
+                                        <td style="border: 1px solid #000; padding: 6px;">
+                                            <input type="text" name="viento2_medida" id="viento2_medida" value="24.00 M" class="form-control form-control-sm" style="width: 150px; display: inline-block; vertical-align: middle; margin: 0 4px;">
+                                        </td>
+                                        <td style="border: 1px solid #000; padding: 6px;">
+                                            <input type="text" name="viento2_colinda" id="viento2_colinda" value="Lote 2" class="form-control form-control-sm" style="width: 150px; display: inline-block; vertical-align: middle; margin: 0 4px;">
+                                        </td>
+                                        </tr>
+                                        <tr>
+                                        <td style="border: 1px solid #000; padding: 6px;">Noreste</td>
+                                        <td style="border: 1px solid #000; padding: 6px;">
+                                            <input type="text" name="viento3_medida" id="viento3_medida" value="24.00 M" class="form-control form-control-sm" style="width: 150px; display: inline-block; vertical-align: middle; margin: 0 4px;">
+                                        </td>
+                                        <td style="border: 1px solid #000; padding: 6px;">
+                                            <input type="text" name="viento3_colinda" id="viento3_colinda" value="Lote 2" class="form-control form-control-sm" style="width: 150px; display: inline-block; vertical-align: middle; margin: 0 4px;">
+                                        </td>
+                                        </tr>
+                                        <tr>
+                                        <td style="border: 1px solid #000; padding: 6px;">Suroeste</td>
+                                        <td style="border: 1px solid #000; padding: 6px;">
+                                            <input type="text" name="viento4_medida" id="viento4_medida" value="24.00 M" class="form-control form-control-sm" style="width: 150px; display: inline-block; vertical-align: middle; margin: 0 4px;">
+                                        </td>
+                                        <td style="border: 1px solid #000; padding: 6px;">
+                                            <input type="text" name="viento4_colinda" id="viento4_colinda" value="Lote 2" class="form-control form-control-sm" style="width: 150px; display: inline-block; vertical-align: middle; margin: 0 4px;">
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+
+
+                            <h4>SEGUNDA. - </h4>
+                            <p>El vendedor se compromete a vender a la parte compradora en 
+                                <input type="text" name="precio_venta_asc" id="precio_venta_asc" value="$348,000.00 (trescientos cuarenta y ocho mil pesos 00/100 m.n)" class="form-control form-control-sm" style="width: 350px; display: inline-block; vertical-align: middle; margin: 0 4px;"> 
+                            mismos que se pagarán de la siguiente manera: 
+                                <input type="text" name="precio_enganche_asc" id="precio_enganche_asc" value="$34,800.00 (treinta y cuatro mil ochocientos pesos 00/100 m.n)" class="form-control form-control-sm" style="width: 350px; display: inline-block; vertical-align: middle; margin: 0 4px;"> 
+                            en concepto de enganche, el resto que es la cantidad de <strong>$313,200.00 (trescientos trece mil doscientos pesos 00/100 m.n)</strong> se pagará en <strong> 24
+                            mensualidades</strong> de <strong>$13,050.00 (trece mil cincuenta pesos 00/100 m.n)</strong>, contados a la firma del presente contrato.</p>
+
+                            <h4>TERCERA.-</h4>
+                            <p>La responsabilidad de obtención de los permisos de subdivisión, lotificación y servicios recaen íntegramente en el comprador.</p>
+
+                            <h4>CUARTA.-</h4>
+                            <p>El comprador se compromete a cumplir con los abonos antes mencionados; en caso de incumplir con el tercer abono perderá los derechos 
+                            de su terreno y no se reintegrarán los pagos realizados.</p>
+
+                            <h4>QUINTA.-</h4>
+                            <p>La escrituración y los gastos que se generen de este mismo quedarán a cargo del comprador.</p>
+
+                            <h4>SEXTA.-</h4>
+                            <p>Ambas partes aceptan el presente contrato en todos y cada uno de sus términos de conformidad, por lo que firman al calce.</p>
+
+                            <h4>SEPTIMA.-</h4>
+                            <p>Ambas partes manifiestan que en el presente contrato no existió error, dolo, mala fe, ni ningún otro vicio de la voluntad que pudiera invalidarlo.</p>
+                            <p class="text-right">Zacapu, Michoacán, a 31 de enero de 2025.</p>
+
+                            <!-- Firmas -->
+                            <table class="tabla-firmas">
+                            <tr>
+                                <td class="celda vacia"></td>
+                                <td class="celda vacia"></td>
+                            </tr>
+                            <tr>
+                                <td class="celda vendedor"> <strong>LA ARQ TANIA MEDINA MARCOS</strong><br>Representante de los Vendedores</td>
+                                <td class="celda comprador"> <strong>CARLOS</strong><br>Comprador </td>
+                            </tr>
+                            </table>
+
+                            <!-- Observaciones -->
+                            <div class="observaciones">
+                            <p><strong>OBSERVACIONES:</strong></p>
+                            <p>Firma como gestor oficio su hija Glenis Maldonado Aburto en representación de Carlos Maldonado.</p>
+                            </div>
+                        </form>
                     </section>
 
                     <!-- Confirm Details -->
-                    <h3> Generación de Contrato </h3>
+                    <h3> Vista Previa del Contrato </h3>
                     <section>
                         <div class="row justify-content-center">
                             <div class="col-lg-6">
@@ -221,16 +424,7 @@
                 </div>
                     
                 </div>
-                <div class="row">
-                    <div class="col-sm-6">
-                        <a href="<?php echo e(route('cliente.index')); ?>" class="btn text-muted d-none d-sm-inline-block btn-link"><i class="mdi mdi-arrow-left me-1"></i> Cancelar </a>
-                    </div> <!-- end col -->
-                    <div class="col-sm-6">
-                        <div class="text-end">
-                            <button type="submit" class="btn btn-success"><i class="mdi mdi-check me-1"></i> Guardar </button>
-                        </div>
-                    </div> 
-                </div>
+                
             </form>
         </div>
     </div>
