cd9b00fe1056982ad239d7bf713ce866c431f099
Luis Jorge (MAC) <gevolutionxalapa@gmail.com>
2025-11-29 21:32:13 -0600

    Usuario tipo 4

--- database/seeders/UserTableSeeder.php
+++ database/seeders/UserTableSeeder.php
@@ -14,8 +14,9 @@ class UserTableSeeder extends Seeder
     public function run(): void
     {
         \DB::table('users')->insert([
-            ['nombre' => "Pablo", 'primer_apellido' => 'Gasparin', 'dob'=>'2000-10-10','email' => 'director@arquitectos.com','password' => Hash::make('12345678'),'email_verified_at'=>'2022-01-02 17:04:58','avatar' => 'images/avatar-1.jpg', 'role_id' => 1, 'estatus_id' => 1, 'fecha_registro' => now(), 'created_at' => now()],
+            ['nombre' => "Luis Jorge", 'primer_apellido' => 'Pablo', 'dob'=>'2000-10-10','email' => 'lpablo@arquitectos.com','password' => Hash::make('12345678'),'email_verified_at'=>'2022-01-02 17:04:58','avatar' => 'images/avatar-1.jpg', 'role_id' => 1, 'estatus_id' => 1, 'fecha_registro' => now(), 'created_at' => now()],
             ['nombre' => "Alberto", 'primer_apellido' => 'Hernández', 'dob'=>'2000-10-10','email' => 'alberto@arquitectos.com','password' => Hash::make('12345678'),'email_verified_at'=>'2022-01-02 17:04:58','avatar' => 'images/avatar-1.jpg', 'role_id' => 4, 'estatus_id' => 1, 'fecha_registro' => now(), 'created_at' => now()],
+            ['nombre' => "Tania", 'primer_apellido' => 'Medina Marcos', 'dob'=>'2000-10-10','email' => 'tania@arquitectos.com','password' => Hash::make('12345678'),'email_verified_at'=>'2022-01-02 17:04:58','avatar' => 'images/avatar-1.jpg', 'role_id' => 4, 'estatus_id' => 1, 'fecha_registro' => now(), 'created_at' => now()],
             // ['nombre' => "Roberto", 'primer_apellido' => 'Gonzáles', 'dob'=>'2000-10-10','email' => 'admin@arquitectos.com','password' => Hash::make('12345678'),'email_verified_at'=>'2022-01-02 17:04:58','avatar' => 'images/avatar-1.jpg', 'role_id' => 3, 'estatus_id' => 1, 'fecha_registro' => now(), 'created_at' => now()],
             // ['nombre' => "Juan Alberto", 'primer_apellido' => 'Morales', 'dob'=>'2000-10-10','email' => 'corredor@arquitectos.com','password' => Hash::make('12345678'),'email_verified_at'=>'2022-01-02 17:04:58','avatar' => 'images/avatar-1.jpg', 'role_id' => 4, 'estatus_id' => 1, 'fecha_registro' => now(), 'created_at' => now()],
         ]);
