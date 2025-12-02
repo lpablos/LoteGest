4026687b2d3cb6526f08e82bde49f93dcb29dbaf
Luis Jorge (MAC) <gevolutionxalapa@gmail.com>
2025-12-01 22:52:30 -0600

    Proteccion de rutas

--- routes/web.php
+++ routes/web.php
@@ -18,50 +18,41 @@ Auth::routes(['verify' => true]);
 
 Route::get('/', [App\Http\Controllers\HomeController::class, 'root'])->name('root')->middleware('auth');
 
-// customers route
-Route::get('/customers', [App\Http\Controllers\CustomerController::class, 'index'])->name('customers.list');
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::get('/customers', [App\Http\Controllers\CustomerController::class, 'index'])->name('customers.list');
+    Route::post('/update-profile/{id}', [App\Http\Controllers\HomeController::class, 'updateProfile'])->name('updateProfile');
+    Route::post('/update-password/{id}', [App\Http\Controllers\HomeController::class, 'updatePassword'])->name('updatePassword');
+    Route::post('guarda-corredor', [App\Http\Controllers\UserController::class, 'storeCorredor'])->name('usuario.corredor');
+    Route::post('duplicar-lote', [App\Http\Controllers\LoteController::class, 'duplicado'])->name('duplicar.lote');
+    Route::post('duplicar-lotfraccionamientoe', [App\Http\Controllers\FraccionamientoController::class, 'duplicado'])->name('duplicar.fraccionamiento');
+    Route::get('cliente/contratos/{idCliente}', [App\Http\Controllers\ClienteController::class, 'verContrato'])->name('cliente.contratos');
+    Route::resource('lote', App\Http\Controllers\LoteController::class);
+    Route::resource('usuarios', App\Http\Controllers\UserController::class);
+    Route::resource('perfiles', App\Http\Controllers\RoleController::class);
+    Route::resource('cliente', App\Http\Controllers\ClienteController::class);
+    Route::get('cliente-compra/{solicitudSys}', [App\Http\Controllers\ClienteController::class, 'continue'])->name('cliente.compra.continue');
+    Route::get('cliente-nueva-compra/{idCliente}', [App\Http\Controllers\ClienteController::class, 'nuevaCompra'])->name('cliente.nueva.compra');
+    Route::resource('estatus', App\Http\Controllers\CatEstatusController::class);
+    Route::resource('proyectos', App\Http\Controllers\ProyectoController::class);
+    Route::resource('contratos', App\Http\Controllers\ContratoController::class);
+    Route::resource('municipios', App\Http\Controllers\CatMunicipioController::class);
+    Route::resource('configuracion', App\Http\Controllers\ConfiguracionController::class);
+    Route::resource('tipo-de-predios', App\Http\Controllers\CatTipoPredioController::class);
+    Route::resource('fraccionamiento', App\Http\Controllers\FraccionamientoController::class);
+    Route::get('fraccionamiento-detalle/{idFracc}', [App\Http\Controllers\FraccionamientoController::class, 'detFracc'])->name('fracc.detalle.registro');
+    Route::get('manzanas-lotes/{idFracc}', [App\Http\Controllers\FraccionamientoController::class, 'fraccManzanaLote'])->name('fracc.Manzana.lote');
+    Route::get('vientos-fraccionamientos/{idFracc}', [App\Http\Controllers\FraccionamientoController::class, 'vientosFraccionamiento'])->name('vientos.fraccion.identy');
+    Route::resource('estatus-proyectos', App\Http\Controllers\CatEstatusProyectoController::class);
+    Route::resource('entidades-federativas', App\Http\Controllers\CatEntidadFederativaController::class);
+    Route::resource('estatus-disponibilidad', App\Http\Controllers\CatEstatusDisponibilidadController::class);
+    Route::resource('compra-fraccionamiento-lotes', App\Http\Controllers\CompraController::class);
+    // Route::resource('compras', App\Http\Controllers\CompraController::class);
+    
+    Route::get('vista-previa-contrato/{registro}', [App\Http\Controllers\DocumentosController::class, 'vistaPreviaContrato']);
+    Route::get('vista-contrato/{registro}', [App\Http\Controllers\DocumentosController::class, 'contratoPDF']);
+    Route::get('ver-documento/{id}', [App\Http\Controllers\DocumentosController::class, 'verDocumento'])->name('constrato.sistema.digital');
+});    
 
-//Update User Details
-Route::post('/update-profile/{id}', [App\Http\Controllers\HomeController::class, 'updateProfile'])->name('updateProfile');
-Route::post('/update-password/{id}', [App\Http\Controllers\HomeController::class, 'updatePassword'])->name('updatePassword');
-
-
-// Route::prefix('gestion-proyectos')->group(function () {
-//      Route::controller(ProyectoController::class)->group(function () {
-//             Route::get('/', 'index')->name('gestion.proyecto.index');
-//             Route::get('formulario-proyecto', 'create')->name('gestion.proyecto.create');
-//      });
-// });
-
-Route::post('guarda-corredor', [App\Http\Controllers\UserController::class, 'storeCorredor'])->name('usuario.corredor');
-Route::post('duplicar-lote', [App\Http\Controllers\LoteController::class, 'duplicado'])->name('duplicar.lote');
-Route::post('duplicar-lotfraccionamientoe', [App\Http\Controllers\FraccionamientoController::class, 'duplicado'])->name('duplicar.fraccionamiento');
-Route::get('cliente/contratos/{idCliente}', [App\Http\Controllers\ClienteController::class, 'verContrato'])->name('cliente.contratos');
-Route::resource('lote', App\Http\Controllers\LoteController::class);
-Route::resource('usuarios', App\Http\Controllers\UserController::class);
-Route::resource('perfiles', App\Http\Controllers\RoleController::class);
-Route::resource('cliente', App\Http\Controllers\ClienteController::class);
-Route::get('cliente-compra/{solicitudSys}', [App\Http\Controllers\ClienteController::class, 'continue'])->name('cliente.compra.continue');
-Route::get('cliente-nueva-compra/{idCliente}', [App\Http\Controllers\ClienteController::class, 'nuevaCompra'])->name('cliente.nueva.compra');
-Route::resource('estatus', App\Http\Controllers\CatEstatusController::class);
-Route::resource('proyectos', App\Http\Controllers\ProyectoController::class);
-Route::resource('contratos', App\Http\Controllers\ContratoController::class);
-Route::resource('municipios', App\Http\Controllers\CatMunicipioController::class);
-Route::resource('configuracion', App\Http\Controllers\ConfiguracionController::class);
-Route::resource('tipo-de-predios', App\Http\Controllers\CatTipoPredioController::class);
-Route::resource('fraccionamiento', App\Http\Controllers\FraccionamientoController::class);
-Route::get('fraccionamiento-detalle/{idFracc}', [App\Http\Controllers\FraccionamientoController::class, 'detFracc'])->name('fracc.detalle.registro');
-Route::get('manzanas-lotes/{idFracc}', [App\Http\Controllers\FraccionamientoController::class, 'fraccManzanaLote'])->name('fracc.Manzana.lote');
-Route::get('vientos-fraccionamientos/{idFracc}', [App\Http\Controllers\FraccionamientoController::class, 'vientosFraccionamiento'])->name('vientos.fraccion.identy');
-Route::resource('estatus-proyectos', App\Http\Controllers\CatEstatusProyectoController::class);
-Route::resource('entidades-federativas', App\Http\Controllers\CatEntidadFederativaController::class);
-Route::resource('estatus-disponibilidad', App\Http\Controllers\CatEstatusDisponibilidadController::class);
-Route::resource('compra-fraccionamiento-lotes', App\Http\Controllers\CompraController::class);
-// Route::resource('compras', App\Http\Controllers\CompraController::class);
-
-Route::get('vista-previa-contrato/{registro}', [App\Http\Controllers\DocumentosController::class, 'vistaPreviaContrato']);
-Route::get('vista-contrato/{registro}', [App\Http\Controllers\DocumentosController::class, 'contratoPDF']);
-Route::get('ver-documento/{id}', [App\Http\Controllers\DocumentosController::class, 'verDocumento'])->name('constrato.sistema.digital');
 
 
 
