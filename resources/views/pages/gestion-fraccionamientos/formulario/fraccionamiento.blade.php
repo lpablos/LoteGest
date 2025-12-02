5007810c7738bbb1e87b345328237ed79507ba16
Luis Jorge (MAC) <gevolutionxalapa@gmail.com>
2025-12-02 13:24:44 -0600

    Mostrar imagen de fraccionamiento

--- resources/views/pages/gestion-fraccionamientos/formulario/fraccionamiento.blade.php
+++ resources/views/pages/gestion-fraccionamientos/formulario/fraccionamiento.blade.php
@@ -1,9 +1,9 @@
 
-    <!-- <div class="row mb-3">
+    <div class="row mb-3">
         <div class="col-md-12 text-center">
-            <img src="" alt="Imagen del fraccionamiento" width="700" height="400">
+            <img src="" alt="Imagen del fraccionamiento" id="imgFracc" width="700" height="400">
         </div>
-    </div> -->
+    </div>
 
 
 <div class="row mb-3">
